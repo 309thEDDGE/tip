874048fa6b7630de68b476d7cf7f5a2f40696e36
Isaac Myers <isaac.myers.2@us.af.mil>
2020-08-31 14:36:24 -0600

    Fix payload count and trailing status word error

--- cpp/ch10/src/ch10_milstd1553f1.cpp
+++ cpp/ch10/src/ch10_milstd1553f1.cpp
@@ -224,7 +224,7 @@ uint8_t Ch10MilStd1553F1::parse_payload_new()
 			// Length in bytes, so divide by 2 to get data word count.
 			// Subtract four (rx, tx, tx stat, rx stat) to get the data word count.
 		*/
-		datum_count = (msg_commword->length / 2) - 4;
+		datum_count = (msg_commword->length / 2) - 3;
 		datum_shift = 3;
 		if (msg_commword->sub_addr2 == 0 || msg_commword->sub_addr2 == 31)
 			command_word_payload_count_ = msg_commword->word_count2 & 0x0010;
@@ -257,7 +257,7 @@ uint8_t Ch10MilStd1553F1::parse_payload_new()
 	{
 		/* [ RX ][ DATA0 ] ... [ DATAN ][ STAT ] */
 
-		datum_count = (msg_commword->length / 2) - 2;
+		datum_count = (msg_commword->length / 2) - 1;
 		datum_shift = 1;
 		if (msg_commword->sub_addr1 == 0 || msg_commword->sub_addr1 == 31)
 			command_word_payload_count_ = msg_commword->word_count1 & 0x0010;
@@ -312,7 +312,10 @@ uint8_t Ch10MilStd1553F1::parse_payload_new()
 	if (datum_count < command_word_payload_count_)
 		is_payload_incomplete_ = 1;
 	else
+	{
+		datum_count = command_word_payload_count_;
 		is_payload_incomplete_ = 0;
+	}
 
 #ifdef LOCALDB
 #ifdef PARQUET
@@ -449,21 +452,40 @@ void Ch10MilStd1553F1::ParsePayloadLibIRIG106()
 {
 	datum = (const uint16_t*)i106_1553msg_.Data;
 
-	// Using the old, often incorrect method of calculating the message
-	// payload count from the message length. This is done to easily compare
-	// with the "truth" data set. In the future, use i106_1553msg_.WordCount instead.
+	// Calculate the message payload count from the message length. 
+	// We are interested in calculating the payload count to know if 
+	// it contains fewer words than expected from the command word.
+	// In the case of BC to RT and RT to RT the series of data words
+	// is followed by a status word. Because the 1553 data are temporally
+	// scheduled, if the 1553 message is short one word it must be the 
+	// final word, or the status word for the two transfer formats mentioned.
+	// Modify the subtracted word count (sum of command and status words)
+	// below to compensate for the trailing status word. Ex: An RT to RT
+	// message with a total payload of 70 bytes does not actually truncate
+	// the data payload in the case of a 32-word expected payload, 
+	// 70/2 - 3 = 32 words. The fact that a RT to RT message with 32 payload
+	// words is not 72 bytes (36 words = 2 comm + 2 status + 32 data) does 
+	// not matter because the data shorage, in this case one word, the final
+	// status word, always occurs at the end of the payload.
+
+	// The subtracted word count mods are: 
+	// * RT to RT: 4 --> 3
+	// * BC to RT: 2 --> 1
 	if (msg_commword->RR)
-		datum_count = (msg_commword->length / 2) - 4;
+		datum_count = (msg_commword->length / 2) - 3;
 	else if (RTtoBC)
 		datum_count = (msg_commword->length / 2) - 2;
 	else
-		datum_count = (msg_commword->length / 2) - 2;
+		datum_count = (msg_commword->length / 2) - 1;
 
 	command_word_payload_count_ = i106_1553msg_.WordCount;
 	if (datum_count < command_word_payload_count_)
 		is_payload_incomplete_ = 1;
 	else
+	{
+		datum_count = command_word_payload_count_;
 		is_payload_incomplete_ = 0;
+	}
 
 #ifdef LOCALDB
 #ifdef PARQUET
