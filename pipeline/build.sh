1c9e65b2aad71510d4f8802d2efa979b65b7fec8
Adam Richards <adam.richards@omitron.com>
2020-08-31 22:14:55 +0000

    Update build.sh just show tools for now

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -1,7 +1,15 @@
+# custom build command which will run in the pipeline
 # in the pipeline the working directory is the root of the project repository
-BUILD_DIR=build
+# BUILD_DIR=build
 
-mkdir -p $BUILD_DIR \
-    && cd $BUILD_DIR \
-    && cmake .. -DCONTAINER=ON \
-    && make -j2 VERBOSE=1
+# mkdir -p $BUILD_DIR \
+#     && cd $BUILD_DIR \
+#     && cmake .. -DCONTAINER=ON \
+#     && make -j2 VERBOSE=1
+
+cmake --version
+gcc --version
+g++ --version
+ctest --version
+ninja --version
+gcovr --version
