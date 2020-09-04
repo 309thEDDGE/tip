fefe7c0c0fb37333b7506749fcf6ad56d0abacae
Isaac Myers <isaac.myers.2@us.af.mil>
2020-09-04 13:16:44 -0600

    Fix casting of generic PDU to specific type

--- cpp/ethernet/include/network_packet_parser.h
+++ cpp/ethernet/include/network_packet_parser.h
@@ -17,17 +17,17 @@ class NetworkPacketParser
 private:
 	uint16_t pdu_type_;
 	const std::map<uint16_t, std::string> pdu_type_to_name_map_ = { {0, "RAW"}, {1, "ETHERNET_II"},
-		{2,"IEEE802_3"}, {3,"RADIOTAP"}, {4, "DOT11"}, {5, "DOT11_ACK"}, {6, "DOT11_ASSOC_REQ"} };
-		/*{7, "DOT11_ASSOC_RESP"}, {8, "DOT11_AUTH"}, {9, "DOT11_BEACON"}, {10, "DOT11_BLOCK_ACK"},
+		{2,"IEEE802_3"}, {3,"RADIOTAP"}, {4, "DOT11"}, {5, "DOT11_ACK"}, {6, "DOT11_ASSOC_REQ"},
+		{7, "DOT11_ASSOC_RESP"}, { 8, "DOT11_AUTH" }, { 9, "DOT11_BEACON" }, { 10, "DOT11_BLOCK_ACK" },
 		{11, "DOT11_BLOCK_ACK_REQ"}, {12, "DOT11_CF_END"}, {13, "DOT11_DATA"}, {14, "DOT11_CONTROL"},
 		{15, "DOT11_DEAUTH"}, {16, "OT11_DIASSOC"}, {17, "DOT11_END_CF_ACK"}, {18, "DOT11_MANAGEMENT"},
 		{19, "DOT11_PROBE_REQ"}, {20, "DOT11_PROBE_RESP"}, {21, "DOT11_PS_POLL"}, {22, "DOT11_REASSOC_REQ"},
 		{23, "DOT11_REASSOC_RESP"}, {24, "DOT11_RTS"}, {25, "DOT11_QOS_DATA"}, {26, "LLC"},
-		{27, "SNAP"}, {28, "IP"}, {29, "ARP"}, {30, "TCP"}, {31 "UDP"}, {32, "ICMP"}, {33, "BOOTP"},
+		{27, "SNAP"}, {28, "IP"}, {29, "ARP"}, {30, "TCP"}, {31, "UDP"}, {32, "ICMP"}, {33, "BOOTP"},
 		{34, "DHCP"}, {35, "EAPOL"}, {36, "RC4EAPOL"}, {37, "RSNEAPOL"}, {38, "DNS"},
 		{39, "LOOPBACK"}, {40, "IPv6"}, {41, "ICMPv6"}, {42, "SLL"}, {43, "DHCPv6"}, {44, "DOT1Q"},
 		{45, "PPPOE"}, {46, "STP"}, {47, "PPI"}, {48, "IPSEC_AH"}, {49, "IPSEC_ESP"}, {50, "PKTAP"},
-		{51, "MPLS"}, {999, "UNKNOWN"}, {1000, "USER_DEFINED_PDU"} });*/
+		{51, "MPLS"}, {999, "UNKNOWN"}, {1000, "USER_DEFINED_PDU"} };
 
 public:
 	NetworkPacketParser();
@@ -35,6 +35,7 @@ public:
 	// 802.3
 	uint8_t ParseEthernetDot3(const uint8_t* buffer, const uint32_t& tot_size);
 
+	void PrintPDUType();
 	//uint8_t ParseUDP(const uint8_t* buffer, const uint32_t& tot_size);
 };
 
--- cpp/ethernet/src/network_packet_parser.cpp
+++ cpp/ethernet/src/network_packet_parser.cpp
@@ -7,25 +7,37 @@ NetworkPacketParser::NetworkPacketParser() : pdu_type_(0)
 
 uint8_t NetworkPacketParser::ParseEthernetDot3(const uint8_t* buffer, const uint32_t& tot_size)
 {
-	
 	Tins::Dot3 dot3(buffer, tot_size);
 
 	// Get Data layer mac addresses.
 	Tins::HWAddress<6> dst_addr = dot3.dst_addr();
 	Tins::HWAddress<6> src_addr = dot3.src_addr();
-	printf("dst: %s, src: %s\n", dst_addr.to_string().c_str(), src_addr.to_string().c_str());
+	printf("dst: %s, src: %s, length: %hu\n", dst_addr.to_string().c_str(), src_addr.to_string().c_str(),
+		dot3.length());
 
-	pdu_type_ = static_cast<uint8_t>(dot3.inner_pdu()->pdu_type());
-	printf("Inner pdu type: %hu, %s\n", pdu_type_, pdu_type_to_name_map_.at(pdu_type_).c_str());
+	Tins::PDU* pdu = dot3.inner_pdu();
+	pdu_type_ = static_cast<uint8_t>(pdu->pdu_type());
+	PrintPDUType();
 
-	// UDP
+	// LLC
 	if (pdu_type_ == 26)
 	{
-		//Tins::UDP* udp = dot3.inner_pdu();
-		//printf("dport is %hu\n", udp->dport());
+		Tins::LLC* llc = dynamic_cast<Tins::LLC*>(pdu);
+		printf("dsap %hhu, ssap %hhu, type %hhu\n", llc->dsap(), llc->ssap(), llc->type());
+		pdu = llc->inner_pdu();
+		pdu_type_ = static_cast<uint16_t>(pdu->pdu_type());
+		PrintPDUType();
+		//Tins::RawPDU* raw = dynamic_cast<Tins::RawPDU*>(pdu);
+		//printf("raw payload length %u\n", raw->payload_size());
+
 	}
+	
 
 	return 0;
 }
 
+void NetworkPacketParser::PrintPDUType()
+{
+	printf("PDU type %hu - %s\n", pdu_type_, pdu_type_to_name_map_.at(pdu_type_).c_str());
+}
 //uint8_t 
