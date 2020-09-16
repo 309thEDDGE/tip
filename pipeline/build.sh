8e35dd149376ec2242b85cedaab00086c7224441
Rick Haws <richard.haws@us.af.mil>
2020-09-16 08:03:28 -0600

    Removed early exit from pipeline/build.sh to allow caching to take place

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -26,12 +26,11 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
-mkdir -p build ; cd build
-touch one
+mkdir -p $BUILD_DIR ; cd $BUILD_DIR ; pwd
+touch two
 ls -last
 find . -name \*.a
-
-exit 0
+cd $BASE_DIR
 
 echo -n "Checking for ninja..."
 if [ -f /usr/local/bin/ninja ] ; then
