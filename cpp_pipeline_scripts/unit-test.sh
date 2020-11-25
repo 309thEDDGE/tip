f1ab8a4724f71af5772e738e36e743e3a13f67e8
Rick Haws <richard.haws@us.af.mil>
2020-11-25 09:54:59 -0700

    Added handling of command-line args for script testing

--- cpp_pipeline_scripts/unit-test.sh
+++ cpp_pipeline_scripts/unit-test.sh
@@ -12,81 +12,110 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # printf an error message before exiting
 trap 'printf "\"${last_command}\" command failed with exit code $?.\n"' ERR
 
+# A command line argument is either a test directory or a log file to use in place of end-to-end testing
+if [ -f "$1" ]; then
+	LOG_FILE=$1
+elif [ -d "$1" ]; then
+	E2E_TEST=$1
+elif [ -z "$1" ]; then
+	if [ -d /test ]; then
+		E2E_TEST=/test
+	else
+		printf "No file or directory specified and there is no /test directory\n"
+		exit 1
+	fi
+else
+	printf "\n'$1' is not a file or directory\n"
+	printf "Usage: unit_test.sh [log_file | test_directory]\n"
+	exit 1
+fi
 
 declare -A MAX_TIMES
 MAX_TIMES=([parse]=0.0 [translate]=0.0)
 
 
 BASE_DIR=${PWD}
-if [ -z "${CMAKE_BUILD_DIR}" ] ; then 
+if [ -z "${CMAKE_BUILD_DIR}" ]; then 
+	LOCAL=True
 	# We are not in the pipeline; set vars for running locally
-	BASE_DIR=/app
+	if [ -d /app ]; then 
+		BASE_DIR=/app
+	fi
 	CMAKE_BUILD_DIR=${BASE_DIR}/build
 	UNITTEST_REPORT_DIR=$BASE_DIR/reports
 fi
 TEST_DIR=${CMAKE_BUILD_DIR}/cpp
 
-# printf "\nUnit Tests\n\n"
+# Skip unit tests if a log file was specified for testing
+if [ -z "$LOG_FILE" ]; then
+	printf "\n-------------------- Unit Tests --------------------\n"
 
-# # For now, run cpp/tests because it is much faster than ctest
-# # In the future we might have to run ctest in order to get coverage statistics
-# # If we do, try to make our tests compatible with the --parallel option of ctest
-# cd ${TEST_DIR}
-# ./tests
-# cd ${BASE_DIR}
+	# For now, run cpp/tests because it is much faster than ctest
+	# In the future we might have to run ctest in order to get coverage statistics
+	# If we do, try to make our tests compatible with the --parallel option of ctest
+	cd ${TEST_DIR}
+	./tests
+	cd ${BASE_DIR}
 
-# printf "\nTest Coverage\n"
+	printf "\n-------------------- Test Coverage --------------------\n"
 
-# mkdir -p ${UNITTEST_REPORT_DIR}
-# printf "Writing coverage reports in ${UNITTEST_REPORT_DIR}\n"
-# GCOV=gcov
-# GCOV="${GCOV}" gcovr -j --verbose \
-#     --exclude-unreachable-branches \
-#     --exclude-throw-branches \
-#     --object-directory="${TEST_DIR}" \
-#     --xml ${UNITTEST_REPORT_DIR}/overall-coverage.xml \
-#     --html ${UNITTEST_REPORT_DIR}/overall-coverage.html \
-#     --sonarqube ${UNITTEST_REPORT_DIR}/overall-coverage-sonar.xml \
-#     --filter "${CPP_COVERAGE_FILTER}" \
-#     $(if [ -n "${CPP_COVERAGE_EXCLUDE}" ]; then printf --exclude="${CPP_COVERAGE_EXCLUDE}\n"; fi)
-
-# printf "\n--------------------Parser validation---------------------\n\n"
-
-# cd $BASE_DIR
-# if [ ! -d ./bin ] ; then mv build/bin . ; fi
-# python tip_scripts/e2e_validation/run_end_to_end_validator.py --video /test/truth /test/test /test/log
-
-printf "\n--------------------End-to-end results------------------\n\n"
-
-# get newest log file
-while [ -z "$E2E_TEST" ] ; do
-	# If in a test container
-	if [ -d /test ] ; then E2E_TEST=/test
-	# If the first remaining command-line arg is a directory
-	elif [ -d "$1" ] ; then E2E_TEST="$1"
-	# If the first remaining command-line arg is a file
+	if [ -x gcovr ]; then
+		mkdir -p ${UNITTEST_REPORT_DIR}
+		printf "Writing coverage reports in ${UNITTEST_REPORT_DIR}\n"
+		GCOV=gcov
+		GCOV="${GCOV}" gcovr -j --verbose \
+			--exclude-unreachable-branches \
+			--exclude-throw-branches \
+			--object-directory="${TEST_DIR}" \
+			--xml ${UNITTEST_REPORT_DIR}/overall-coverage.xml \
+			--html ${UNITTEST_REPORT_DIR}/overall-coverage.html \
+			--sonarqube ${UNITTEST_REPORT_DIR}/overall-coverage-sonar.xml \
+			--filter "${CPP_COVERAGE_FILTER}" \
+			$([ -n "${CPP_COVERAGE_EXCLUDE}" ] && printf --exclude="${CPP_COVERAGE_EXCLUDE}\n")
+	else 
+		printf "*** No gcovr found. Skipping test coverage ***\n"
+		if [ -z "$LOCAL" ]; then
+			exit 1
+		fi
 	fi
-done
 
-if [ -f "$1" ] ; then LOG_FILE=$1
-else LOG_FILE="$(ls -1t $E2E_TEST/log/* | head -1)"; fi
+	printf "\n-------------------- End-to-End Test ---------------------\n"
+	cd $BASE_DIR
+	# In the pipeline, only the build directory is present.
+	# Restore the bin directory if needed.
+	if [ ! -d ./bin ]; then mv build/bin . ; fi
 
-if [[ -z "$LOG_FILE" ]] ; then
-	printf "No log files were found.\n"
+	# If in a test container
+	if [ -z "E2E_TEST" ]; then
+		printf "No test directory specified and directory /test does not exist"
+		EXIT $1
+	fi
+
+	# Run end-to-end validator
+	python tip_scripts/e2e_validation/run_end_to_end_validator.py --video $E2E_TEST/truth $E2E_TEST/test $E2E_TEST/log
+
+	# get newest log file
+	LOG_FILE="$(ls -1t $E2E_TEST/log/* | head -1)"
+
+else
+	printf "\n*** Log file specified on command line.  Skipping tests. ***\n"
+fi
+
+
+printf "\n-------------------- End-to-End Results ------------------\n"
+
+if [[ -z "$LOG_FILE" ]]; then
+	printf "FAIL: No log file was found.\n"
 	exit 1
 fi
 
 EXIT_CODE=0 # Start by assuming success
-if grep "Total raw 1553 data: PASS" "$LOG_FILE" ; then
-	printf "Parser validation succeeded\n"
-else
+if ! grep "Total raw 1553 data: PASS" "$LOG_FILE" ; then
 	printf "ERROR: Parser validation failed\n"
 	EXIT_CODE=1
 fi
 
-if grep "Total translated 1553 data: PASS" "$LOG_FILE" ; then
-	printf "Translator validation succeeded\n"
-else
+if ! grep "Total translated 1553 data: PASS" "$LOG_FILE" ; then
 	printf "ERROR: Translator validation failed\n"
 	# Continue without failing; the pipeline doesn't expect translation to succeed at this point
 fi
@@ -106,7 +135,7 @@ function check_time {
 	# printf "\n${#words[*]} words \n${words[*]} \n"
 
 	# Get the word before "seconds"
-	if [ ${#words[*]} -gt 1 ] ; then 
+	if [ ${#words[*]} -gt 1 ]; then 
 		value="${words[-2]}"
 	fi
 	# printf "Value: $value\n"
@@ -116,9 +145,9 @@ function check_time {
 		printf "None\n"
 	else
 		# Use python to compare floating point values
-		if [ $(python -c "print($value < $threshold)") != "True" ] ; then
+		if [ $(python -c "print($value < $threshold)") != "True" ]; then
 			return_value=1
-			printf "FAIL\n"
+			printf "FAIL: $value is not less than $threshold seconds\n"
 		else
 			printf "PASS\n"
 		fi
@@ -129,10 +158,10 @@ function check_time {
 
 # Check times
 for type in ${!MAX_TIMES[@]} ; do
-	printf "\nChecking $type time: \n"
+	printf "\n$type time: "
 	if ! check_time $LOG_FILE $type ${MAX_TIMES[$type]} ; then
 		# For now, do not fail for translate time
-		if [ "$type" != "Translate" ] ; then
+		if [ "$type" != "Translate" ]; then
 			EXIT_CODE=1
 		fi
 	fi
