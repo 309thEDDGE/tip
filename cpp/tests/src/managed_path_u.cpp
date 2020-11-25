3b9a5ccc8042dd3f2f1b86f2cfaa7093d264b331
Isaac Myers <isaac.myers.2@us.af.mil>
2020-11-24 18:01:52 -0700

    Fix tests, use os-independent path creation

--- cpp/tests/src/managed_path_u.cpp
+++ cpp/tests/src/managed_path_u.cpp
@@ -3,114 +3,94 @@
 #include "managed_path.h"
 #include <fstream>
 
+bool HasWindowsPrefix(std::string input_str)
+{
+	char backslash = '\\';
+	char q_mark = '?';
+	if (input_str[0] == backslash && input_str[1] == backslash
+		&& input_str[2] == q_mark && input_str[3] == backslash)
+	{
+		return true;
+	}
+	return false;
+}
+
 TEST(ManagedPathTest, AmendPathInsertsPrefix)
 {
 	ManagedPath mp;
-	std::string prefix = "\\\\?\\";
 
 	// < 260
-	std::string test_path_str = R"(C:\User\Is\not-here\ok.txt)";
-	std::filesystem::path test_path(test_path_str);
+	std::vector<std::string> test_path_str = { "data", "Is", "not-here", "ok.txt" };
 
-	EXPECT_TRUE(test_path_str.size() < 260);
-	EXPECT_EQ(mp.AmendPath(test_path).string(), test_path_str);
+	// Build the path in a platform-independent way
+	for (auto s : test_path_str)
+		mp /= s;
+	std::filesystem::path test_path(mp.RawString());
+
+	EXPECT_TRUE(test_path.string().size() < 260);
+	EXPECT_FALSE(HasWindowsPrefix(mp.AmendPath(test_path).string()));
 
 	// = 260
-	test_path_str = std::string("C:\\Users\\this\\is\\the\\path\\to-the-file\\and_it_must_be"
-		"_equivalent_to_260_chars_in_size\\this-is-a-long-part01\\"
-		"this-is-a-long-part02\\this-is-a-long-part03\\this-is-a-long-part04\\"
-		"this-is-a-long-part05\\this-is-a-long-part06\\this-is-a-long-part07\\"
-		"this-is-a-long-aa.txt");
-	//printf("string len: %zu\n", test_path_str.size());
-	test_path = std::filesystem::path(test_path_str);
-	EXPECT_TRUE(test_path_str.size() == 260);
-
-	EXPECT_EQ(mp.AmendPath(test_path).string(), test_path_str);
+	test_path_str = std::vector<std::string>({ "this","path","to-the-file","must_be",
+		"_equivalent_to_260_chars_in_size", "this-is-a-long-part01",
+		"this-is-a-long-part02", "this-is-a-long-part03\\this-is-a-long-part04",
+		"this-is-a-long-part05", "this-is-a-long-part06", "this-is-a"});
+	mp = ManagedPath();
+	for (auto s : test_path_str)
+		mp /= s;
+	//printf("string len: %zu\n", mp.RawString().size());
+	test_path = std::filesystem::path(mp.RawString());
+	EXPECT_TRUE(test_path.string().size() == 260);
+	EXPECT_FALSE(HasWindowsPrefix(mp.AmendPath(test_path).string()));
 
 	// > 260
-	test_path_str = std::string("C:\\Users\\this\\is\\the\\path\\to-the-file\\and_it_must_be"
-		"_equivalent_to_260_chars_in_size\\this-is-a-long-part01\\"
-		"this-is-a-long-part02\\this-is-a-long-part03\\this-is-a-long-part04\\"
-		"this-is-a-long-part05\\this-is-a-long-part06\\this-is-a-long-part07\\"
-		"this-is-a-long-damn-file.txt");
-	test_path = std::filesystem::path(test_path_str);
-	EXPECT_TRUE(test_path_str.size() > 260);
-#ifdef __WIN64
-	EXPECT_EQ(mp.AmendPath(test_path).string(), prefix + test_path_str);
-#elif defined __linux__
-	EXPECT_EQ(mp.AmendPath(test_path).string(), test_path_str);
-#endif
-
-	test_path_str = std::string("/this_is_another/very_long_path/down_the_rabbit_hole01/"
-		"down_the_rabbit_hole02/down_the_rabbit_hole03/"
-		"down_the_rabbit_hole04/down_the_rabbit_hole05/"
-		"down_the_rabbit_hole06/down_the_rabbit_hole07/"
-		"down_the_rabbit_hole08/down_the_rabbit_hole09/"
-		"down_the_rabbit_hole10/down_the_rabbit_hole11/"
-		"to_the_final_dir");
-	test_path = std::filesystem::path(test_path_str);
-	EXPECT_TRUE(test_path_str.size() > 260);
-#ifdef __WIN64
-	EXPECT_EQ(mp.AmendPath(test_path).string(), prefix + test_path_str);
-#elif defined __linux__
-	EXPECT_EQ(mp.AmendPath(test_path).string(), test_path_str);
-#endif
+	test_path_str = std::vector<std::string>({ "this","path","to-the-file","must_be",
+		"_equivalent_to_260_chars_in_size", "this-is-a-long-part01",
+		"this-is-a-long-part02", "this-is-a-long-part03\\this-is-a-long-part04",
+		"this-is-a-long-part05", "this-is-a-long-part06", "this-is-another-very-long-part" });
+	mp = ManagedPath();
+	for (auto s : test_path_str)
+		mp /= s;
+	test_path = std::filesystem::path(mp.RawString());
+	EXPECT_TRUE(test_path.string().size() > 260);
+	EXPECT_TRUE(HasWindowsPrefix(mp.AmendPath(test_path).string()));
 }
 
 TEST(ManagedPathTest, string)
 {
 	
-	std::string prefix = "\\\\?\\";
+	ManagedPath mp;
 
 	// < 260
-	std::string test_path_str = R"(C:\User\Is\not-here\ok.txt)";
-	ManagedPath mp(test_path_str);
-	std::filesystem::path test_path(test_path_str);
+	std::vector<std::string> test_path_str = { "data", "Is", "not-here", "ok.txt" };
 
-	EXPECT_TRUE(test_path_str.size() < 260);
-	EXPECT_EQ(mp.string(), test_path_str);
+	// Build the path in a platform-independent way
+	for (auto s : test_path_str)
+		mp /= s;
+	EXPECT_TRUE(mp.RawString().size() < 260);
+	EXPECT_FALSE(HasWindowsPrefix(mp.string()));
 
 	// = 260
-	test_path_str = std::string("C:\\Users\\this\\is\\the\\path\\to-the-file\\and_it_must_be"
-		"_equivalent_to_260_chars_in_size\\this-is-a-long-part01\\"
-		"this-is-a-long-part02\\this-is-a-long-part03\\this-is-a-long-part04\\"
-		"this-is-a-long-part05\\this-is-a-long-part06\\this-is-a-long-part07\\"
-		"this-is-a-long-aa.txt");
-	test_path = std::filesystem::path(test_path_str);
-	mp = ManagedPath(test_path_str);
-	EXPECT_TRUE(test_path_str.size() == 260);
-	EXPECT_EQ(mp.string(), test_path_str);
+	test_path_str = std::vector<std::string>({ "this","path","to-the-file","must_be",
+		"_equivalent_to_260_chars_in_size", "this-is-a-long-part01",
+		"this-is-a-long-part02", "this-is-a-long-part03\\this-is-a-long-part04",
+		"this-is-a-long-part05", "this-is-a-long-part06", "this-is-a" });
+	mp = ManagedPath();
+	for (auto s : test_path_str)
+		mp /= s;
+	EXPECT_TRUE(mp.RawString().size() == 260);
+	EXPECT_FALSE(HasWindowsPrefix(mp.string()));
 
 	// > 260
-	test_path_str = std::string("C:\\Users\\this\\is\\the\\path\\to-the-file\\and_it_must_be"
-		"_equivalent_to_260_chars_in_size\\this-is-a-long-part01\\"
-		"this-is-a-long-part02\\this-is-a-long-part03\\this-is-a-long-part04\\"
-		"this-is-a-long-part05\\this-is-a-long-part06\\this-is-a-long-part07\\"
-		"this-is-a-long-damn-file.txt");
-	test_path = std::filesystem::path(test_path_str);
-	mp = ManagedPath(test_path_str);
-	EXPECT_TRUE(test_path_str.size() > 260);
-#ifdef __WIN64
-	EXPECT_EQ(mp.string(), prefix + test_path_str);
-#elif defined __linux__
-	EXPECT_EQ(mp.string(), test_path_str);
-#endif
-
-	test_path_str = std::string("/this_is_another/very_long_path/down_the_rabbit_hole01/"
-		"down_the_rabbit_hole02/down_the_rabbit_hole03/"
-		"down_the_rabbit_hole04/down_the_rabbit_hole05/"
-		"down_the_rabbit_hole06/down_the_rabbit_hole07/"
-		"down_the_rabbit_hole08/down_the_rabbit_hole09/"
-		"down_the_rabbit_hole10/down_the_rabbit_hole11/"
-		"to_the_final_dir");
-	test_path = std::filesystem::path(test_path_str);
-	mp = ManagedPath(test_path_str);
-	EXPECT_TRUE(test_path_str.size() > 260);
-#ifdef __WIN64
-	EXPECT_EQ(mp.string(), prefix + test_path_str);
-#elif defined __linux__
-	EXPECT_EQ(mp.string(), test_path_str);
-#endif
+	test_path_str = std::vector<std::string>({ "this","path","to-the-file","must_be",
+		"_equivalent_to_260_chars_in_size", "this-is-a-long-part01",
+		"this-is-a-long-part02", "this-is-a-long-part03\\this-is-a-long-part04",
+		"this-is-a-long-part05", "this-is-a-long-part06", "this-is-another-very-long-part" });
+	mp = ManagedPath();
+	for (auto s : test_path_str)
+		mp /= s;
+	EXPECT_TRUE(mp.RawString().size() > 260);
+	EXPECT_TRUE(HasWindowsPrefix(mp.string()));
 }
 
 TEST(ManagedPathTest, CreateDirectoryAlreadyExists)
@@ -216,74 +196,44 @@ TEST(ManagedPathTest, CreateDirectoryLongPath)
 //	EXPECT_TRUE(fs::remove_all(root_path));
 //}
 
-
-#ifdef __WIN64
 TEST(ManagedPathTest, AppendOperator)
 {
 	// /=
-	std::string s1 = "C:\\Users\\my";
-	std::string s2 = "in\\here-you-go";
+	std::string s1 = "data";
+	std::string s2 = "path";
 	ManagedPath mp1(s1);
 	ManagedPath mp2(s2);
+	std::filesystem::path p1(s1);
+	std::filesystem::path p2(s2);
+	p1 /= p2;
 	mp1 /= mp2;
-	EXPECT_EQ(mp1.RawString(), s1 + "\\" + s2);
+	EXPECT_EQ(mp1.RawString(), p1.string());
 }
 
 TEST(ManagedPathTest, ConcatenateOperator)
 {
 	// /
-	std::string s1 = "C:\\Users\\my";
-	std::string s2 = "in\\here-you-go";
+	std::string s1 = "data";
+	std::string s2 = "path";
 	ManagedPath mp1(s1);
 	ManagedPath mp2(s2);
+	std::filesystem::path p1(s1);
+	std::filesystem::path p2(s2);
 	ManagedPath mp = mp1 / mp2;
-	EXPECT_EQ(mp.RawString(), s1 + "\\" + s2);
+	std::filesystem::path p = p1 / p2;
+	EXPECT_EQ(mp.RawString(), p.string());
 }
 
 TEST(ManagedPathTest, AppendNoSeparator)
 {
 	// /
-	std::string s1 = "C:\\Users\\my";
-	std::string s2 = "in\\here-you-go";
+	std::string s1 = "data";
+	std::string s2 = "path";
 	ManagedPath mp1(s1);
 	ManagedPath mp2(s2);
+	std::filesystem::path p1(s1);
+	std::filesystem::path p2(s2);
 	mp1 += mp2;
-	EXPECT_EQ(mp1.RawString(), s1 + s2);
+	p1 += p2;
+	EXPECT_EQ(mp1.RawString(), p1.string());
 }
-
-
-
-#elif defined __linux__
-TEST(ManagedPathTest, AppendOperator)
-{
-	// /=
-	std::string s1 = "/Users/my";
-	std::string s2 = "in/here-you-go";
-	ManagedPath mp1(s1);
-	ManagedPath mp2(s2);
-	mp1 /= mp2;
-	EXPECT_EQ(mp1.RawString(), s1 + "/" + s2);
-}
-
-TEST(ManagedPathTest, ConcatenateOperator)
-{
-	// /
-	std::string s1 = "/Users/my";
-	std::string s2 = "in/here-you-go";
-	ManagedPath mp1(s1);
-	ManagedPath mp2(s2);
-	ManagedPath mp = mp1 / mp2;
-	EXPECT_EQ(mp.RawString(), s1 + "/" + s2);
-}
-
-TEST(ManagedPathTest, AppendNoSeparator)
-{
-	// /
-	std::string s1 = "/Users/my";
-	std::string s2 = "in/here-you-go";
-	ManagedPath mp1(s1);
-	ManagedPath mp2(s2);
-	mp1 += mp2;
-	EXPECT_EQ(mp1.RawString(), s1 + s2);
-}
-#endif
