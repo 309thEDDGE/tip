077d4b6b24b772fa1f7fb60b9a275945134fc293
Rick Haws <richard.haws@us.af.mil>
2020-11-25 10:21:10 -0700

    Restored "echo" for readability and "which" for correctness

--- cpp_pipeline_scripts/unit-test.sh
+++ cpp_pipeline_scripts/unit-test.sh
@@ -9,8 +9,8 @@ set -e
 
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# printf an error message before exiting
-trap 'printf "\"${last_command}\" command failed with exit code $?.\n"' ERR
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
 # A command line argument is either a test directory or a log file to use in place of end-to-end testing
 if [ -f "$1" ]; then
@@ -21,12 +21,13 @@ elif [ -z "$1" ]; then
 	if [ -d /test ]; then
 		E2E_TEST=/test
 	else
-		printf "No file or directory specified and there is no /test directory\n"
+		echo "No file or directory specified and there is no /test directory"
 		exit 1
 	fi
 else
-	printf "\n'$1' is not a file or directory\n"
-	printf "Usage: unit_test.sh [log_file | test_directory]\n"
+	echo ""
+	echo "'$1' is not a file or directory"
+	echo "Usage: unit_test.sh [log_file | test_directory]"
 	exit 1
 fi
 
@@ -48,7 +49,8 @@ TEST_DIR=${CMAKE_BUILD_DIR}/cpp
 
 # Skip unit tests if a log file was specified for testing
 if [ -z "$LOG_FILE" ]; then
-	printf "\n-------------------- Unit Tests --------------------\n"
+	echo ""
+	echo "-------------------- Unit Tests --------------------"
 
 	# For now, run cpp/tests because it is much faster than ctest
 	# In the future we might have to run ctest in order to get coverage statistics
@@ -57,11 +59,12 @@ if [ -z "$LOG_FILE" ]; then
 	./tests
 	cd ${BASE_DIR}
 
-	printf "\n-------------------- Test Coverage --------------------\n"
+	echo ""
+	echo "-------------------- Test Coverage --------------------"
 
-	if [ -x gcovr ]; then
+	if which gcovr >& /dev/null; then
 		mkdir -p ${UNITTEST_REPORT_DIR}
-		printf "Writing coverage reports in ${UNITTEST_REPORT_DIR}\n"
+		echo "Writing coverage reports in ${UNITTEST_REPORT_DIR}"
 		GCOV=gcov
 		GCOV="${GCOV}" gcovr -j --verbose \
 			--exclude-unreachable-branches \
@@ -71,15 +74,16 @@ if [ -z "$LOG_FILE" ]; then
 			--html ${UNITTEST_REPORT_DIR}/overall-coverage.html \
 			--sonarqube ${UNITTEST_REPORT_DIR}/overall-coverage-sonar.xml \
 			--filter "${CPP_COVERAGE_FILTER}" \
-			$([ -n "${CPP_COVERAGE_EXCLUDE}" ] && printf --exclude="${CPP_COVERAGE_EXCLUDE}\n")
+			$([ -n "${CPP_COVERAGE_EXCLUDE}" ] && echo -n --exclude="${CPP_COVERAGE_EXCLUDE}")
 	else 
-		printf "*** No gcovr found. Skipping test coverage ***\n"
+		echo "*** No gcovr found. Skipping test coverage ***"
 		if [ -z "$LOCAL" ]; then
 			exit 1
 		fi
 	fi
 
-	printf "\n-------------------- End-to-End Test ---------------------\n"
+	echo ""
+	echo "-------------------- End-to-End Test ---------------------"
 	cd $BASE_DIR
 	# In the pipeline, only the build directory is present.
 	# Restore the bin directory if needed.
@@ -87,7 +91,7 @@ if [ -z "$LOG_FILE" ]; then
 
 	# If in a test container
 	if [ -z "E2E_TEST" ]; then
-		printf "No test directory specified and directory /test does not exist"
+		echo "No test directory specified and directory /test does not exist"
 		EXIT $1
 	fi
 
@@ -98,25 +102,28 @@ if [ -z "$LOG_FILE" ]; then
 	LOG_FILE="$(ls -1t $E2E_TEST/log/* | head -1)"
 
 else
-	printf "\n*** Log file specified on command line.  Skipping tests. ***\n"
+	echo ""
+	echo "*** Log file specified on command line.  Skipping tests. ***"
 fi
 
 
-printf "\n-------------------- End-to-End Results ------------------\n"
+echo ""
+echo "-------------------- End-to-End Results ------------------"
 
 if [[ -z "$LOG_FILE" ]]; then
-	printf "FAIL: No log file was found.\n"
+	echo "FAIL: No log file was found."
 	exit 1
 fi
 
 EXIT_CODE=0 # Start by assuming success
 if ! grep "Total raw 1553 data: PASS" "$LOG_FILE" ; then
-	printf "ERROR: Parser validation failed\n"
+	echo "ERROR: Parser validation failed"
 	EXIT_CODE=1
 fi
 
+echo ""
 if ! grep "Total translated 1553 data: PASS" "$LOG_FILE" ; then
-	printf "ERROR: Translator validation failed\n"
+	echo "ERROR: Translator validation failed"
 	# Continue without failing; the pipeline doesn't expect translation to succeed at this point
 fi
 
@@ -126,30 +133,35 @@ function check_time {
 	threshold=$1; shift
 	return_value=0 # Assume success
 	# Use the first 5 characters of the type in the pattern (e.g. Parse, Transl)
-	pattern=$(printf "${key_word:0:5}.*:.*seconds")
-	# printf "\nFile: $file \nPattern: $pattern \nThreshold: $threshold\n"
+	pattern=$(echo "${key_word:0:5}.*:.*seconds")
+	# echo ""
+	# echo "File: $file "
+	# echo "Pattern: $pattern "
+	# echo "Threshold: $threshold"
 
 	# Get the line containing the total time
 	# and save an array of the words
 	words=( $(grep -ie "$pattern" $file) )
-	# printf "\n${#words[*]} words \n${words[*]} \n"
+	# echo ""
+	# echo "${#words[*]} words "
+	# echo "${words[*]} "
 
 	# Get the word before "seconds"
 	if [ ${#words[*]} -gt 1 ]; then 
 		value="${words[-2]}"
 	fi
-	# printf "Value: $value\n"
+	# echo "Value: $value"
 
 	if [ "$value" = "None" ]; then
 		return_value=1
-		printf "None\n"
+		echo "None"
 	else
 		# Use python to compare floating point values
 		if [ $(python -c "print($value < $threshold)") != "True" ]; then
 			return_value=1
-			printf "FAIL: $value is not less than $threshold seconds\n"
+			echo "FAIL: $value is not less than $threshold seconds"
 		else
-			printf "PASS\n"
+			echo "PASS"
 		fi
 	fi
 
@@ -158,7 +170,8 @@ function check_time {
 
 # Check times
 for type in ${!MAX_TIMES[@]} ; do
-	printf "\n$type time: "
+	echo ""
+	echo -n "$type time: "
 	if ! check_time $LOG_FILE $type ${MAX_TIMES[$type]} ; then
 		# For now, do not fail for translate time
 		if [ "$type" != "Translate" ]; then
