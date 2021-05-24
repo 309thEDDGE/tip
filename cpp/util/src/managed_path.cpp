0f852d263ac3e5e1062b9cea6f89bb2c81f2a0a4
Isaac Myers <isaac.myers.2@us.af.mil>
2021-05-24 13:07:50 -0600

    Fix, remove unneccessary cast

--- cpp/util/src/managed_path.cpp
+++ cpp/util/src/managed_path.cpp
@@ -23,7 +23,7 @@ ManagedPath::ManagedPath(std::initializer_list<std::string> path_components)
 	fs::path temp_path = fs::current_path();
 	for (auto comp : path_components)
 	{
-		temp_path /= std::string(comp);
+		temp_path /= comp;
 	}
 	this->assign(temp_path);
 }
