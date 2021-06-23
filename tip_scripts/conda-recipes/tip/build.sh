818a118b3ae42423af1c968b3b14135eca3ccb0d
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-06-23 12:36:17 -0400

    Reverting changes to CMake_linux.txt and cpp/CMakeLists.txt

--- tip_scripts/conda-recipes/tip/build.sh
+++ tip_scripts/conda-recipes/tip/build.sh
@@ -6,10 +6,9 @@ cd build-release
 cmake ${CMAKE_ARGS} .. \
     -G Ninja \
     -DCONTAINER=ON \
+    -DCONDA=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 cmake --build . --target install
-
-
