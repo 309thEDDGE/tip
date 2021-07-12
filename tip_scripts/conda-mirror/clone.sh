31917e9dc734959372e056d61a9ab5b3e2827b64
t-potts <tylerjacobpotts@gmail.com>
2021-07-12 16:50:50 -0500

    [OPAL]-132 Update mirror-config to reflect conda-forge packages

--- tip_scripts/conda-mirror/clone.sh
+++ tip_scripts/conda-mirror/clone.sh
@@ -6,13 +6,13 @@ main () {
 	    --target-directory $CONDA_MIRROR_DIR \
 	    --platform linux-64 \
 	    --config $MIRROR_CONFIG \
-	    --upstream-channel https://repo.anaconda.com/pkgs/main/
+	    --upstream-channel https://conda.anaconda.org/conda-forge/
 	
 	conda-mirror \
 	    --target-directory $CONDA_MIRROR_DIR \
 	    --platform noarch \
 	    --config $MIRROR_CONFIG \
-	    --upstream-channel https://repo.anaconda.com/pkgs/main/ 
+	    --upstream-channel https://conda.anaconda.org/conda-forge/
 }
 
 main $@
