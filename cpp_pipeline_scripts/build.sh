1e52148b095d609306914a93233e16be74561c5b
t-potts <tylerjacobpotts@gmail.com>
2021-07-15 16:37:25 -0500

    try using default directory for conda build

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -8,7 +8,7 @@ main() {
     setup
     export MINICONDA3_PATH="/home/user/miniconda3"
     export ARTIFACT_DIR="${ARTIFACT_FOLDER}/build-metadata/build-artifacts"
-    export CONDA_CHANNEL_DIR="/local-channel"
+    # export CONDA_CHANNEL_DIR="/local-channel" 
     export PATH="$MINICONDA3_PATH/bin:${PATH}"
 
     # mkdir $ARTIFACT_DIR
@@ -30,8 +30,7 @@ main() {
     echo -n "Building tip"
     ./conda_build.sh
 
-    tar -cvf local_channel.tar /local-channel
-    cp local_channel.tar $ARTIFACT_DIR/
+
 }
 
 if ! is_test ; then 
