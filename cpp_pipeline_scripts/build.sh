0a231bd0feb6a02e8d83b6f6221440d75c177d19
Tyler Potts <tpotts@quansight.com>
2021-07-15 20:47:10 +0000

    Set local channel parent directory to current working directory

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -7,40 +7,29 @@ main() {
 	set_exit_on_error
 	setup
     export MINICONDA3_PATH="/home/user/miniconda3"
-    export CONDA_CHANNEL_DIR="/local-channel"
+    export CONDA_CHANNEL_DIR="$(pwd)/local-channel"
     export PATH="$MINICONDA3_PATH/bin:${PATH}"
     export ARTIFACT_DIR="${ARTIFACT_FOLDER}/build-metadata/build-artifacts"
     # mkdir $ARTIFACT_DIR
     #if [[ ! -d ${CMAKE_BUILD_DIR} ]]; then mkdir $CMAKE_BUILD_DIR; fi 
-    echo "===========printenv============"
-    printenv
 
-    echo "===========pwd=========="
-    pwd
 
-    # echo -n "Installing Miniconda"
-    # dnf install wget -y
-    # wget --progress=dot:giga \
-    #      https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-    #      && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
+    echo -n "Installing Miniconda"
+    dnf install wget -y
+    wget --progress=dot:giga \
+         https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+         && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
 
-    # echo -n "Installing conda-build"
-    # conda install conda-build -y
-    # echo -n "Change directory to conda-build recipes"
-    # cd tip_scripts
-    # echo -n "Building tip"
-    # ./conda_build.sh
+    echo -n "Installing conda-build"
+    conda install conda-build -y
+    echo -n "Change directory to conda-build recipes"
+    cd tip_scripts
+    echo -n "Building tip"
+    ./conda_build.sh
 
-    # cp -r $CONDA_CHANNEL_DIR/ $CMAKE_BUILD_DIR/
-    # ls $CONDA_CHANNEL_DIR
-    # echo "cmake build dir: $CMAKE_BUILD_DIR"
-    
 
-    # ls /
-    # ls /$CONDA_CHANNEL_DIR
-    # ls /$CONDA_CHANNEL_DIR/linux-64
-    # cp -r $CONDA_CHANNEL_DIR/linux-64/*tip* $ARTIFACT_DIR/
-    # ls $ARTIFACT_DIR
+    cp -r $CONDA_CHANNEL_DIR $ARTIFACT_DIR/
+    ls $ARTIFACT_DIR
 }
 
 if ! is_test ; then 
