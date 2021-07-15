cf26ce11437b825e891cb7e148e1c300ed0c4540
t-potts <tylerjacobpotts@gmail.com>
2021-07-15 17:19:03 -0500

    back to basics

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -8,60 +8,58 @@ main() {
     setup
     export MINICONDA3_PATH="/home/user/miniconda3"
     export ARTIFACT_DIR="${ARTIFACT_FOLDER}/build-metadata/build-artifacts"
-    # export CONDA_CHANNEL_DIR="/local-channel"
+    export CONDA_CHANNEL_DIR="/local-channel"
     export PATH="$MINICONDA3_PATH/bin:${PATH}"
+
+    mkdir $CONDA_CHANNEL_DIR
+    echo "proof2" > $CONDA_CHANNEL_DIR/test_cache.txt
+
+    cp -r $CONDA_CHANNEL_DIR/ $ARTIFACT_DIR/
+
+
 
     # mkdir $ARTIFACT_DIR
     #if [[ ! -d ${CMAKE_BUILD_DIR} ]]; then mkdir $CMAKE_BUILD_DIR; fi 
-    echo "============ls current dir======="
-    ls -al
 
-    echo "========pwd of starting dir========"
-    pwd
-
-    echo "===========ls -al ../ from starting dir============="
-    ls -al ../ 
-
-    echo "==========echo local channel dir========="
-    echo $CONDA_CHANNEL_DIR
-
-    echo -n "Installing Miniconda"
-    dnf install wget -y
-    wget --progress=dot:giga \
-         https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-         && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
-
-    echo -n "Installing conda-build"
-    conda install conda-build -y
-    echo -n "Change directory to conda-build recipes"
-    cd tip_scripts
-    echo -n "Building tip"
-    ./conda_build.sh
-
-    # tar -cvf local_channel.tar /local-channel
     
-    echo "creating artifact dir if not already present"
-    if [[ ! -d ${ARTIFACT_DIR} ]]; then mkdir -p $ARTIFACT_DIR; fi
 
-    echo "listing channel that was created"
-    ls -al /home/user/miniconda3/conda-bld/linux-64
+    # echo -n "Installing Miniconda"
+    # dnf install wget -y
+    # wget --progress=dot:giga \
+    #      https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    #      && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
 
-    echo "list with wildcard for tar"
-    ls -al /home/user/miniconda3/conda-bld/linux-64/*.tar*
+    # echo -n "Installing conda-build"
+    # conda install conda-build -y
+    # echo -n "Change directory to conda-build recipes"
+    # cd tip_scripts
+    # echo -n "Building tip"
+    # ./conda_build.sh
+
+    # # tar -cvf local_channel.tar /local-channel
     
-    echo "attempting copy of tarball"
-    cp /home/user/miniconda3/conda-bld/linux-64/*.tar* $ARTIFACT_DIR/
+    # echo "creating artifact dir if not already present"
+    # if [[ ! -d ${ARTIFACT_DIR} ]]; then mkdir -p $ARTIFACT_DIR; fi
 
-    echo "list artifact dir"
-    ls $ARTIFACT_DIR
+    # echo "listing channel that was created"
+    # ls -al /home/user/miniconda3/conda-bld/linux-64
 
-    echo "copying test text file"
-    echo "proof positive" > test.txt
-    cp test.txt $ARTIFACT_DIR
+    # echo "list with wildcard for tar"
+    # ls -al /home/user/miniconda3/conda-bld/linux-64/*.tar*
+    
+    # echo "attempting copy of tarball"
+    # cp /home/user/miniconda3/conda-bld/linux-64/*.tar* $ARTIFACT_DIR/
 
-    echo "copying test tar file"
-    tar -cvf test_tar.tar test.txt
-    cp test_tar.tar $ARTIFACT_DIR
+    # echo "list artifact dir"
+    # ls $ARTIFACT_DIR
+
+    # echo "copying test text file"
+    # echo "proof positive" > test.txt
+    # cp test.txt $ARTIFACT_DIR
+
+    # echo "copying test tar file"
+    # tar -cvf test_tar.tar test.txt
+    # cp test_tar.tar $ARTIFACT_DIR
 }
 
 if ! is_test ; then 
