ea3c2fa7b62394c0880860c8e1380c475b41e616
t-potts <tylerjacobpotts@gmail.com>
2021-07-16 12:24:41 -0500

    WORKING! Removing testing lines

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -13,19 +13,9 @@ main() {
     export SCRIPT_START_DIR=$(pwd)
 
     echo "current working directory at start of script"
-    pwd
-    
-    mkdir test_dir
-    echo "proof1" > test_dir/test_cache.txt
+    pwd    
 
     mkdir -p $ARTIFACT_DIR
-    cp -r test_dir/ $ARTIFACT_DIR/
-
-
-    # mkdir $ARTIFACT_DIR
-    #if [[ ! -d ${CMAKE_BUILD_DIR} ]]; then mkdir $CMAKE_BUILD_DIR; fi 
-
-    
 
     echo -n "Installing Miniconda"
     dnf install wget -y
@@ -39,64 +29,17 @@ main() {
     cd tip_scripts
     echo -n "Building tip"
     ./conda_build.sh
-    echo "print current working directory after script runs"
-    pwd
 
-    echo "print current working dir after changing back to start"
     cd $SCRIPT_START_DIR
-    pwd
 
-    echo "testing copy of second test file"
-    echo "proof2" > test_dir/test_cache2.txt
-
-    cp -r test_dir/test_cache2.txt $ARTIFACT_DIR/
-
-    echo "testing ability to save tar files"
-    tar -cvf test_dir.tar ./test_dir
-    cp test_dir.tar $ARTIFACT_DIR
-
-    echo "listing artifact DIR after test tar copy"
-    ls -hl $ARTIFACT_DIR
-    
-    # mkdir $CONDA_CHANNEL_DIR
-    # echo "proof2" > $CONDA_CHANNEL_DIR/test_cache.txt
-
-    # cp /home/user/miniconda3/conda-bld/linux-64/click-8.0.1-py38_0.tar.bz2 $ARTIFACT_DIR/
-    # cp $CONDA_CHANNEL_DIR/test_cache.txt $ARTIFACT_DIR/
-    
-    tar -cvf local_channel.tar /local-channel
-
-    echo "listing and grepping for local_channel.tar"
-    ls -hl | grep "local_channel.tar"
+    echo "tarballing files from local channel dir"
+    tar -cvf local_channel.tar $CONDA_CHANNEL_DIR
 
     echo "copying tarball to artifact dir"
     cp local_channel.tar $ARTIFACT_DIR
 
     echo "show all contents of artifact dir"
     ls -hl $ARTIFACT_DIR
-    
-    # echo "creating artifact dir if not already present"
-    # if [[ ! -d ${ARTIFACT_DIR} ]]; then mkdir -p $ARTIFACT_DIR; fi
-
-    # echo "listing channel that was created"
-    # ls -al /home/user/miniconda3/conda-bld/linux-64
-
-    # echo "list with wildcard for tar"
-    # ls -al /home/user/miniconda3/conda-bld/linux-64/*.tar*
-    
-    # echo "attempting copy of tarball"
-    # cp /home/user/miniconda3/conda-bld/linux-64/*.tar* $ARTIFACT_DIR/
-
-    # echo "list artifact dir"
-    # ls $ARTIFACT_DIR
-
-    # echo "copying test text file"
-    # echo "proof positive" > test.txt
-    # cp test.txt $ARTIFACT_DIR
-
-    # echo "copying test tar file"
-    # tar -cvf test_tar.tar test.txt
-    # cp test_tar.tar $ARTIFACT_DIR
 }
 
 if ! is_test ; then 
