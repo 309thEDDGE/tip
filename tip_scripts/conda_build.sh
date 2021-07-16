9f363da4d33d7c92db722e119a347ab539efd3a0
t-potts <tylerjacobpotts@gmail.com>
2021-07-16 11:48:42 -0500

    correct conda channel root for previous test

--- tip_scripts/conda_build.sh
+++ tip_scripts/conda_build.sh
@@ -9,8 +9,8 @@ main() {
     conda index $CONDA_CHANNEL_DIR -s linux-64 -s noarch
     echo -n "Building recipes"
     cd $SCRIPT_PATH/conda-recipes
-    # conda build deleteme-click -c conda-forge --croot $CONDA_CHANNEL_DIR
-    conda build deleteme-click -c conda-forge
+    conda build deleteme-click -c conda-forge --croot $CONDA_CHANNEL_DIR
+    # conda build deleteme-click -c conda-forge
 }
 
 main $@
