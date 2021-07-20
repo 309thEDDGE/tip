dac4496786b7cbede076326f9a799567afaf44e0
t-potts <tylerjacobpotts@gmail.com>
2021-07-20 10:06:06 -0500

    comment out code to test cloning the repo

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -12,34 +12,34 @@ main() {
     export PATH="$MINICONDA3_PATH/bin:${PATH}"
     export SCRIPT_START_DIR=$(pwd)
 
-    echo "current working directory at start of script"
-    pwd    
+    # echo "current working directory at start of script"
+    # pwd    
 
-    mkdir -p $ARTIFACT_DIR
+    # mkdir -p $ARTIFACT_DIR
 
-    echo -n "Installing Miniconda"
-    dnf install wget -y
-    wget --progress=dot:giga \
-         https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-         && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
+    # echo -n "Installing Miniconda"
+    # dnf install wget -y
+    # wget --progress=dot:giga \
+    #      https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    #      && bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINICONDA3_PATH
 
-    echo -n "Installing conda-build"
-    conda install conda-build -y
-    echo -n "Change directory to conda-build recipes"
-    cd tip_scripts
-    echo -n "Building tip"
-    ./conda_build.sh
+    # echo -n "Installing conda-build"
+    # conda install conda-build -y
+    # echo -n "Change directory to conda-build recipes"
+    # cd tip_scripts
+    # echo -n "Building tip"
+    # ./conda_build.sh
 
-    cd $SCRIPT_START_DIR
+    # cd $SCRIPT_START_DIR
 
-    echo "tarballing files from local channel dir"
-    tar -cvf local_channel.tar $CONDA_CHANNEL_DIR
+    # echo "tarballing files from local channel dir"
+    # tar -cvf local_channel.tar $CONDA_CHANNEL_DIR
 
-    echo "copying tarball to artifact dir"
-    cp local_channel.tar $ARTIFACT_DIR
+    # echo "copying tarball to artifact dir"
+    # cp local_channel.tar $ARTIFACT_DIR
 
-    echo "show all contents of artifact dir"
-    ls -hl $ARTIFACT_DIR
+    # echo "show all contents of artifact dir"
+    # ls -hl $ARTIFACT_DIR
 }
 
 if ! is_test ; then 
