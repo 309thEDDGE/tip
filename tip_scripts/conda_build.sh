82d16ce8b9af792caa838d5fa1aae41186c07fdc
t-potts <tylerjacobpotts@gmail.com>
2021-08-02 12:15:53 -0500

    Copy tip package into fresh channel

--- tip_scripts/conda_build.sh
+++ tip_scripts/conda_build.sh
@@ -6,12 +6,15 @@ DIRECTORY_WHEN_EXECUTED=$(pwd)
 main() {
     
     echo -n "Setting up conda build channel..."
-    # mkdir -p $CONDA_CHANNEL_DIR
-    
-    conda index $CONDA_CHANNEL_DIR -s linux-64 -s noarch
+   
     echo -n "Building recipes"
     cd $SCRIPT_PATH/conda-recipes
-    conda build tip -c conda-forge --croot $CONDA_CHANNEL_DIR
+    conda build tip -c conda-forge --croot /tmp-channel
+
+    mkdir -p $CONDA_CHANNEL_DIR/linux-64
+    mkdir -p $CONDA_CHANNEL_DIR/noarch
+    mv /tmp-channel/linux-64/*tip*tar* $CONDA_CHANNEL_DIR/linux-64/
+    conda index $CONDA_CHANNEL_DIR -s linux-64 -s noarch
 
     cd $DIRECTORY_WHEN_EXECUTED
 }
