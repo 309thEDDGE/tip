e706dc81c517e7a874b4983ca1b632982829a016
t-potts <tylerjacobpotts@gmail.com>
2021-08-09 17:01:11 -0500

    [OPAL-49] Update jupyterlab to address CVE. Corrected file size check

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -40,8 +40,8 @@ main() {
     echo "tarballing files from local channel dir"
     tar -cvf local_channel.tar $CONDA_CHANNEL_DIR
 
-    # Raises error if tip channel tarball less than 10MB
-    if (($(stat --printf="%s" local_channel.tar) < 10000000)); then
+    # Raises error if tip channel tarball less than 1MB
+    if (($(stat --printf="%s" local_channel.tar) < 1000000)); then
 	echo "Tip channel tarball less than 1MB."
 	echo "It is likely that the build failed silently. Exiting."
 	exit 1
