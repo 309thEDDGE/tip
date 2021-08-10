bd8bb9e5001d27407c83f263c3294fde35b63adc
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-08-10 10:25:12 -0400

    Adding ctest to the build phase

--- cpp_pipeline_scripts/build.sh
+++ cpp_pipeline_scripts/build.sh
@@ -36,6 +36,7 @@ main() {
 
     conda run -n tip-dev cmake .. -GNinja -DCONDA_PREFIX=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_INSTALL_LIBDIR=lib
     conda run -n tip-dev cmake --build . --target install
+    conda run -n tip-dev ctest
     popd
 
     # ===========================
