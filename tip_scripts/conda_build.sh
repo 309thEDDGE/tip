e6a0e161c76c21f4bc9aa259c2292cd0170fbdcd
Ahmad Bilal <abilal@metrostarsystems.com>
2021-11-04 13:22:43 -0400

    fixed tmp-channel directory permission by placing tmp-channel at HOME

--- tip_scripts/conda_build.sh
+++ tip_scripts/conda_build.sh
@@ -4,16 +4,16 @@ SCRIPT_PATH=$(dirname $0)
 DIRECTORY_WHEN_EXECUTED=$(pwd)
 
 main() {
-    
+
     echo -n "Setting up conda build channel..."
-   
+
     echo -n "Building recipes"
     cd $SCRIPT_PATH/conda-recipes
-    conda build tip -c conda-forge --croot /tmp-channel
+    conda build tip -c conda-forge --croot ${HOME}/tmp-channel
 
     mkdir -p $CONDA_CHANNEL_DIR/linux-64
     mkdir -p $CONDA_CHANNEL_DIR/noarch
-    mv /tmp-channel/linux-64/*tip*tar* $CONDA_CHANNEL_DIR/linux-64/
+    mv ${HOME}/tmp-channel/linux-64/*tip*tar* $CONDA_CHANNEL_DIR/linux-64/
     conda index $CONDA_CHANNEL_DIR -s linux-64 -s noarch
 
     cd $DIRECTORY_WHEN_EXECUTED
