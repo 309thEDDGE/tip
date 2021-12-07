18afd3e2e053c2d800b63a672b0a00473feddb14
dglowe <danielglowe95@gmail.com>
2021-12-07 16:49:02 -0600

    Stubbed out some tests

--- cpp/tests/src/dts429_u.cpp
+++ cpp/tests/src/dts429_u.cpp
@@ -1,10 +1,24 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include "dts429.h"
+// #include "dts429.h"
+
+
+TEST(DTS429Test, ReadYaml)
+{
+
+    EXPECT_FALSE(true);
+}
 
 
 
+TEST(DTS429Test, YamlParsedCorrectly)
+{
 
+    EXPECT_FALSE(true);
+}
 
+TEST(DTS429Test, StoreParsedInMap)
+{
 
-
+    EXPECT_FALSE(true);
+}
