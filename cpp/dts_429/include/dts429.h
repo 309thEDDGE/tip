9fdc3657ceb52cd58b25f0432a70ffd22a9b02b7
dglowe <danielglowe95@gmail.com>
2022-01-26 15:57:08 -0600

    Removed commented code, updating comments

--- cpp/dts_429/include/dts429.h
+++ cpp/dts_429/include/dts429.h
@@ -36,48 +36,20 @@ const std::map<std::string, ICDElementSchema> StringToICDElementSchemaMap = {
 // The primary data of interest is a representation of the Interface Control
 // Document (ICD). These data are managed by the ICDData class.
 //
-// Data supplemental to bus mapping and which include 429 Labels of
-// words which may be present in the raw 429 parquet tables that are not
-// included in the 429 message descriptions are processed by the
-// SupplementalBusMapLabels class.
-//
-// Currently a yaml file, containing message definitions and supplemental bus
-// map labels, is allowed as input.
+// Currently a vector of strings containing the lines from the 429 DTS and
+// a map for newly created ICD elements to be stored (with word name as key)
+// is the allowed input. Processing begins with IngestLines().
 
 class DTS429
 {
    private:
-   /*
-    // Ingest and manipulate ICD data
-    ICDData icd_data_;
-    ICDData* icd_data_ptr_;
-    std::vector<std::string> yaml_lines_;
-    std::unordered_map<std::string, std::vector<ICDElement>> word_name_to_elements_map_;
-    */
     // Map the top-level DTS1553 yaml file key string to a DTS1553Component
     const std::map<std::string, DTS429Component> yaml_key_to_component_map_ = {
         {"supplemental_bus_map_labels", DTS429Component::SUPPL_BUSMAP_LABELS},
         {"translatable_word_definitions", DTS429Component::TRANSL_WORD_DEFS}};
 
-    // Fill with supplemental bus map labels data if present in the
-    // yaml file. The word key is an integer created by upshifting the 429
-    // label by 8 bits and adding bus number from the IPDH. The key is
-    // the bus name on which the 429 labels + bus numbers used to create the
-    // mapped set occur.
-    std::map<std::string, std::set<uint32_t>> suppl_bus_name_to_word_key_map_;
-
    public:
     DTS429(){}
-    /*
-    DTS429() : icd_data_(), icd_data_ptr_(&icd_data_), yaml_lines_() {}
-    ICDData GetICDData() { return icd_data_; }
-    ICDData* ICDDataPtr() { return icd_data_ptr_; }
-    const std::vector<std::string>& GetYamlLines() { return yaml_lines_; }
-    */
-    std::map<std::string, std::set<uint32_t>> GetSupplBusNameToWordKeyMap()
-    {
-        return suppl_bus_name_to_word_key_map_;
-    }
 
     /*
 		IngestLines
@@ -172,48 +144,6 @@ class DTS429
                                        const YAML::Node& wrd_data,
                                        const YAML::Node& elem_data,
                                        ICDElement& arinc_param);
-
-    /*
-		OpenYamlFile
-
-		dts_path:	Full path to dts file. File name is used determine file type,
-		            either yaml or text/csv. The input file is opened and all data
-                    is stored individually as new line terminated strings in
-                    yaml_lines_.
-
-        word_elements:       Map of ARINC 429 word name to vector of all elements
-                                associated with it (as ICDElment)
-
-		return:		True if success, false if failure.
-
-	*/
-    // bool OpenYamlFile(const ManagedPath& dts_path,
-    //                   std::unordered_map<std::string, std::vector<ICDElement>> word_elements);
-
-    /*
-
-		FillSupplBusNameToWordKeyMap
-
-		suppl_busmap_labels_node:			    Yaml node containing maps with
-												keys corresponding bus names and
-												values as sequences of integers
-												representing ARINC 429 labels.
-
-		output_suppl_busname_to_wrd_key_map:	Output maps the bus name to a set
-												of 429 word keys, where a word
-												key is an integer representing an
-                                                ARINC label that is associated with
-                                                the bus name.
-
-		return:									True if node is empty or has valid
-												structure (maps of strings to
-												sequences of integer values)
-                                                and false otherwise.
-												Output map is empty if node is empty
-												or return value is false.
-	*/
-    // bool FillSupplBusNameToWordKeyMap(const YAML::Node& suppl_busmap_labels_node,
-    //                                  std::map<std::string, std::set<uint32_t>>& output_suppl_busname_to_wrd_key_map);
 
 };
 
