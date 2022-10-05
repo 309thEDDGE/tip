bb5eeccd3f949e0aeb625e5b96f534faec6aba76
Isaac Myers <isaacjmyersphd@gmail.com>
2022-10-05 11:23:58 -0600

    Change cmake config to use parent dir

--- conda-recipes/tip/build.sh
+++ conda-recipes/tip/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} . \
+cmake ${CMAKE_ARGS} .. \
     -G Ninja \
-    -DCMAKE_BUILD_TYPE=Profile \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCI_COMMIT_TAG=$CI_COMMIT_TAG \
     -DCI_COMMIT_SHORT_SHA=$CI_COMMIT_SHORT_SHA
 
 cmake --build . --target install
 
-cd $PREFIX/bin
