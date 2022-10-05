85fe29b56e39fc16068fe0ac24ebf151eb0f9bf1
IM-USAF <91748573+IM-USAF@users.noreply.github.com>
2022-10-05 10:45:15 -0600

    Fix parquet header (#7)
    * Fix pq sync, null
    
    * Fix pq sync, null for win
    
    * Remove old github build dockerfile
    
    * Add LICENSE
    
    * Remove clutter, e2e_validation, gitlab CI, old
    
    * Remove parse_and_translatec_cli.py
    
    * Update README, remove singleuser references
    
    * Fix gitignore, keep conda-recipes
    
    Co-authored-by: Isaac Myers <isaacjmyersphd@gmail.com>

---
+++ conda-recipes/tip/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cmake ${CMAKE_ARGS} . \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Profile \
+    -DCI_COMMIT_TAG=$CI_COMMIT_TAG \
+    -DCI_COMMIT_SHORT_SHA=$CI_COMMIT_SHORT_SHA
+
+cmake --build . --target install
+
+cd $PREFIX/bin
---
+++ conda-recipes/yaml-cpp/build_static.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd build
+
+# Configure step
+cmake .. \
+    -GNinja \
+    -DYAML_BUILD_SHARED_LIBS=OFF \
+    -DYAML_CPP_BUILD_TESTS=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+
+# Build step
+ninja install
