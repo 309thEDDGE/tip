e81bac055dda511e2612d4cb8f710574374eccd2
Isaac Myers <isaac.myers.2@us.af.mil>
2025-09-26 14:26:59 -0600

    Fix config-time definition of NEWARROW21 macro for conda

--- cmake/CMake_linux.cmake
+++ cmake/CMake_linux.cmake
@@ -18,8 +18,9 @@ find_package(Arrow CONFIG REQUIRED)
 # The logic to define NEWARROW21 below will only work after find_package(Arrow...) is called.
 if(${Arrow_VERSION} VERSION_GREATER_EQUAL "21.0")
 	add_definitions(-DNEWARROW21)
-	remove_definitions(-DNEWARROW)
 	message(STATUS "Define macro NEWARROW21: arrow version >= 21.0")
+else()
+	add_definitions(-DNEWARROW)
 endif()
 
 set(GTEST_LIBRARIES GTest::gtest GTest::gmock GTest::gtest_main)
